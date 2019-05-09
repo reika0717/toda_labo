bc1c9624ec039e38fe5447dc2b681e2b35d027f8
reika0717 <reikahirahara@gmail.com>
2019-05-09 12:39:58 +0900

    add ajax code

--- assets/templates/js/main.js
+++ assets/templates/js/main.js
@@ -9,4 +9,18 @@ $(function() {
     NewUrl = CurUrl.replace("studies", "en/studies");
   }
   $("#eng a").attr("href", NewUrl);
+
+  // https://spreadsheets.google.com/feeds/cells/1tS9IKv0vphga9-MnUEzLFCuB2I32s1yiG2e2XE4pjQk/od6/public/values?alt=json //学術論文
+  // https://spreadsheets.google.com/feeds/cells/1tS9IKv0vphga9-MnUEzLFCuB2I32s1yiG2e2XE4pjQk/oi9egak/public/values?alt=json　//ニュース
+  // https://spreadsheets.google.com/feeds/cells/1tS9IKv0vphga9-MnUEzLFCuB2I32s1yiG2e2XE4pjQk/ot9j46w/public/values?alt=json //国際会議
+
+  // 例
+  // $.ajax({
+  //   url: "https://spreadsheets.google.com/feeds/cells/1JGvXRqvu5A5IhaYfz40yTblNP7bZZL6GaPGaZl7knHM/od6/public/values?alt=json",
+  //   dataType: "json",
+  //   async: true,
+  //   success: function(data) {
+  //     //処理
+  //   }
+  // })
 });
